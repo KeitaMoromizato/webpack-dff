9594486103137a9f0a9d358e2c077021fafabbbd
Keita Moromizato <keita.moromi@gmail.com>
2017-06-25 22:52:20 +0900

    get diff

--- index.ts
+++ index.ts
@@ -1,2 +1,12 @@
 import { exec } from 'child_process';
 
+const prevCommit = '^HEAD';
+const targetCommit = 'HEAD';
+
+const getDiffFiles = (targetCommit, prevCommit) => new Promise((resolve, reject) => {
+  exec(`git diff --name-only ${targetCommit} ${prevCommit}`, (error, stdout, stderr) => {
+    if (error) return reject(new Error(stderr));
+
+    resolve(stdout.split('\n'));
+  });
+})
